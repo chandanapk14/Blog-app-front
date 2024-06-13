dbb38d2970a5da08ad011e8d5fbed947127bbcf9
chandanapk14 <chandanapushkaran@gmail.com>
2024-06-13 16:40:22 +0530

    project completed

--- src/components/New.jsx
+++ src/components/New.jsx
@@ -51,7 +51,7 @@ const New = () => {
                             <center><button className="btn btn-primary" onClick={readValue}>Register</button></center>
                             </div>
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                                <center><Link to='/signin'>Sign In</Link></center>
+                                <center><Link to='/'>Sign In</Link></center>
                             </div>
                         </div>
                     </div>
