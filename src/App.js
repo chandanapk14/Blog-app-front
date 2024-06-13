1c77372fa28c995bc29bfda22ddbdcb62136855d
chandanapk14 <chandanapushkaran@gmail.com>
2024-06-13 15:40:03 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Signin from './components/Signin';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SignUp from './SignUp';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path='/' element={<Signin/>}/>
+      <Route path='/signin' element={<Signin/>}/>
+      <Route path='/signup' element={<SignUp/>}/>
       </Routes></BrowserRouter>
   );
 }
